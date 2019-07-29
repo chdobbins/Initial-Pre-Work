9afb2816dba7bf3b68763103806689c4fe58be48
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-29 13:29:56 -0600

    step 6 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+// This lesson taught me how to crate camel case varables
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
