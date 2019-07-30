86925efe07843a0d20f50c89089a7c7efad8b177
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:48:24 -0600

    Step 37 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+// This lesson taught me how to use nested arrays to store values
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Star Wars", 50],["Star Trek", 50]];
