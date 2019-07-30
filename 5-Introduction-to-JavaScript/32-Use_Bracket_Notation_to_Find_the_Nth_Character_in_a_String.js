81d9aa834d6a7d9c0d1f62d5c03a72a8c07444d5
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:07:33 -0600

    Step 32 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// This lesson taught me how to use [#] to find letter locations in a varable
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
