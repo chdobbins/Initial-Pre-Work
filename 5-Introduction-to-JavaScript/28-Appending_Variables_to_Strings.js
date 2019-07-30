397d22161f1a36bafa1f31b368ce8e4ce2e2a1dc
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 14:55:09 -0600

    Step 28 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+// This lesson taught me how to concatenate a string using the += and variables 
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "the greatest";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
