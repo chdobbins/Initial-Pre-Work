e743d8182ebb1dd5acd08e486eb81fba37670b42
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:10:34 -0600

    STep 33 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// This lesson taught me how to use .length - 1 to find last letter locations in a varable
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
