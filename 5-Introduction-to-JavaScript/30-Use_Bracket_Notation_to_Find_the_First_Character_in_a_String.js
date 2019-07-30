8b5d6242539d2b94590ceb5c90998331c076e811
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:01:29 -0600

    step 30 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,13 @@
+// This lesson taught me to use .length to find letter locations in a varable
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
