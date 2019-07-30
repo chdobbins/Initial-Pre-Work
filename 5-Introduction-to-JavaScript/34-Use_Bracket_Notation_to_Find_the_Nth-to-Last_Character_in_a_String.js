ecb879a49eec389aa9e8c709c97604309083cd47
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:12:34 -0600

    Step 34 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// This lesson taught me how to use .length - # to find last letter locations in a varable
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
