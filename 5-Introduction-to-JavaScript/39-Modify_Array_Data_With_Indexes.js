284e83c6187db7d3e0918f71d0c15a23da97eff6
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:58:46 -0600

    Step 39 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,11 @@
+// This lesson taught me how to use []location in arrays to change a value
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+myArray[0] = 45; //equals [45,64,99]
