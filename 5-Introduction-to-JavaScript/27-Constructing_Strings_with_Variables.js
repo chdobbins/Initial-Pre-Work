bd829faf69342bc5c19f965056b37c53cf1ad8b8
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 14:48:02 -0600

    Step 27 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+// This lesson taught me how to concatenate a string using the + and variables 
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName ="Christopher";
+var myStr = "My name is " + myName + "and I am well!";
