e642ea672c45d315a2648f3e26a6d99fdb2dbe84
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 14:57:59 -0600

    Step 29 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,14 @@
+// This lesson taught me to use .length to find how long a varable is
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
