1e1c201aeb66df5d73f65e869fcaff15169900fa
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:43:52 -0600

    Step 36 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+// This lesson taught me how to use arrays to store values
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["The answer to Life", 42];
