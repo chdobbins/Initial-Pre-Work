9576eafde9887dc615139da3dc2d193985267e94
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:05:05 -0600

    Step 31 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+// This lesson taught me that invidual section of varable cannot be changed = all or none
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
