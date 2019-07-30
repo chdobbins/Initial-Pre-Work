312b3bda9ced908e329ef25ab0c85b8578b9a838
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 15:39:18 -0600

    Step 35 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,15 @@
+// This lesson taught me how to use +varable+ to add words to a sentance
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var myNoun = "dog";
+  var myAdjective = "big"
+  var myVerb = "ran"
+  var myAdverb ="quickly"
+  var result = "The " +myAdjective+ " " +myNoun+ " " +myVerb+ "very " +myAdverb+ "up the hill";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
