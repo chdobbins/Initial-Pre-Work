e68d37f588a08872af392d80bf4cc4d74af1d1fa
Christopher Dobbins <cdobbins@nspirehealth.com>
2019-07-30 14:23:04 -0600

    Step 21 of lesson five completed

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+// This lesson taught me how to use string varables
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Christopher";
+var myLastName = "Dobbins";
